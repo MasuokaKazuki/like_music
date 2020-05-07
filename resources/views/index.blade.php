31b0a0ead30b3dbafa4422a2c352f2830dc7ca3f
KazukiMasuoka <masuoka.kazuki@gmail.com>
2020-05-07 16:08:43 +0900

    モックコーディング、まだ途中だけど一旦コミット

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,10 +2,11 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
         <title>Like Music</title>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">    <!-- Fonts -->
         <link rel="stylesheet" href="{{ asset('/common/css/main.css') }}">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     </head>
     <body class="top-page">
         <div class="top-content">
@@ -17,10 +18,11 @@
             <div class="search">
                 <form action="#" method="get">
                     <input class="search__input" type="text" name="artist" placeholder="好きなアーティスト名を入力して探そう" />
-                    <input class="search__button" type="submit" value="見つける" />
+                    <button type="submit" class="search__button">
+                        見つける <i class="fa fa-search"></i>
+                    </button>
                 </form>
             </div>
         </div>
-        <!--<img src="{{ asset('/common/image/top-mainvisual.jpg') }}" />-->
     </body>
 </html>
