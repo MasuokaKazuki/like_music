2a5e0d78405a7cca717e3f74978a0a8506e40a6a
KazukiMasuoka <masuoka.kazuki@gmail.com>
2020-09-19 22:13:25 +0900

    react.js webpack babelの環境を構築

---
+++ frontend/development.js
@@ -0,0 +1,54 @@
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const src  = path.resolve(__dirname, 'src')
+const dist = path.resolve(__dirname, 'dist')
+
+export default {
+  mode: 'development',
+  entry: src + '/jsx/index.jsx',
+
+  output: {
+    path: dist,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+    {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+    },
+    {
+        test: /\.scss/,
+        use: [
+            'style-loader',
+            {
+                loader: 'css-loader',
+                options: {
+                    url: false
+                },
+            },
+            {
+                loader: 'sass-loader',
+                options: {
+                    sourceMap: true,
+                }
+            }
+        ]
+    }
+    ]
+    },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: src + '/index.html',
+        filename: 'index.html',
+    })
+  ]
+}
