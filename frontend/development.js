182126085936a413fb896e21772bd678019e5b8b
KazukiMasuoka <masuoka.kazuki@gmail.com>
2020-10-18 22:42:20 +0900

    async/awaitを使えるように@babel/polyfillをインストール

--- frontend/development.js
+++ frontend/development.js
@@ -6,7 +6,7 @@ const dist = path.resolve(__dirname, 'dist')
 
 export default {
   mode: 'development',
-  entry: src + '/jsx/index.jsx',
+  entry: ['@babel/polyfill', src + '/jsx/index.jsx'],
 
   output: {
     path: dist,
